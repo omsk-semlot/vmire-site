bdb442ad4bf9890bbfc2279f3e643100fa9a1690
Leon Nikitin (@robin-bol.ru) <Leon.V.Nikitin@pravmail.ru>
2022-07-14 15:10:14 +0300

    Добавили пакеты из файлы из gatsby-crafty-theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,43 @@
 module.exports = {
   siteMetadata: {
     title: `alex`,
+    titleTemplate: "%s",
+    description: `We're on a mission to make the web beautiful.`,
+    image: "/logo.png",
+    twitterUsername: "@Simplecodehq",
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+	  "gatsby-plugin-postcss", 
+	  "gatsby-plugin-image", 
+	  "gatsby-plugin-sharp", 
+	  "gatsby-transformer-sharp", 
+          `gatsby-plugin-react-helmet`,
+          `gatsby-transformer-json`,
+	  {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+              "name": "images",
+               path: `./src/data/`,
+              //"path": "./src/images/"
+            },
+            __key: "images"
+          },
+          {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: `gatsby-starter-default`,
+              short_name: `starter`,
+              start_url: `/`,
+              background_color: `#663399`,
+              theme_color: `#663399`,
+              display: `minimal-ui`,
+            },
+          },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
 };
+
+}
